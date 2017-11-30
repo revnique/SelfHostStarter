ad638ac8154b6db5a350e6fac4bb136d4f39514f
revnique <revnique@h1teq.com>
2017-11-30 14:37:53 -0600

    added static hosting... so you can provide services and static files... one stop shop

--- App_Start/Startup.cs
+++ App_Start/Startup.cs
@@ -1,6 +1,11 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.IO;
+using System.Web.Http;
+using Microsoft.Owin;
 using Owin;
 using Microsoft.Owin.Cors;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
 
 namespace SelfHostStarter
 {
@@ -10,6 +15,24 @@ namespace SelfHostStarter
         {
             // Configure Web API for self-host. 
             var config = new HttpConfiguration();
+
+            //added static file hosting to the 
+            string root = AppDomain.CurrentDomain.BaseDirectory;
+            root = root.Replace("\\bin\\Debug", "").Replace("\\bin\\Release", "");
+            root = root.Replace("\\obj\\Debug", "").Replace("\\obj\\Release", "");
+            var physicalFileSystem = new PhysicalFileSystem(Path.Combine(root, "wwwroot"));
+            var options = new FileServerOptions
+            {
+                RequestPath = PathString.Empty,
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = false;
+            app.UseFileServer(options);
+
+
+
 
             app.UseCors(CorsOptions.AllowAll);
             WebApiConfig.Register(config);
